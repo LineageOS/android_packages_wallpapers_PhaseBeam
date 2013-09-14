61687442fbb7a9f06513eba122a7f8cb63b03097
Danny Baumann <dannybaumann@web.de>
2013-09-14 23:05:43 +0100

    Allow phase beam color configuration.
    Credits go to Guillaume Lesniak for the shader algorithm idea.
    
    Change-Id: Id249d6888ba74eae80f18069fd170d6b349b9edf

--- res/raw/bg_fs.glsl
+++ res/raw/bg_fs.glsl
@@ -1,5 +1,24 @@
 varying lowp vec4 color;
+varying vec3 adjust;
+
+// http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
 
 void main() {
-    gl_FragColor = color;
+    vec3 rgb = color.rgb;
+
+    if (adjust.x >= 0.0) {
+        // rgb is already greyscale in that case
+        float value = max(rgb.r, max(rgb.g, rgb.b));
+        vec3 hsv = adjust * vec3(1.0, 1.0, value);
+        rgb = hsv2rgb(hsv);
+    }
+
+    // output pixel color
+    gl_FragColor = vec4(rgb, color.a);
 }
