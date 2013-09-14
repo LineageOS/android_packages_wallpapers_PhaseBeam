d735ff1e76c6b8abc026a02551bde7f01782dd4d
Danny Baumann <dannybaumann@web.de>
2013-09-14 23:05:43 +0100

    Allow phase beam color configuration.
    Credits go to Guillaume Lesniak for the shader algorithm idea.
    
    Change-Id: Id249d6888ba74eae80f18069fd170d6b349b9edf

--- res/raw/bg_fs.glsl
+++ res/raw/bg_fs.glsl
@@ -1,5 +1,26 @@
 varying lowp vec4 color;
+varying vec3 adjust;
+
+// inspired by http://www.chilliant.com/rgb2hsv.html
+vec3 hsl2rgb(vec3 hsl)
+{
+    float r = abs(hsl.x * 6.0 - 3.0) - 1.0;
+    float g = 2.0 - abs(hsl.x * 6.0 - 2.0);
+    float b = 2.0 - abs(hsl.x * 6.0 - 4.0);
+    vec3 rgb = clamp(vec3(r, g, b), 0.0, 1.0);
+    float chroma = (1.0 - abs(2.0 * hsl.z - 1.0)) * hsl.y;
+    return (rgb - 0.5) * chroma + hsl.z;
+}
 
 void main() {
-    gl_FragColor = color;
+    vec3 rgb = color.rgb;
+
+    if (adjust.x >= 0.0) {
+        // rgb is already greyscale in that case, so r = g = b
+        vec3 hsl = adjust * vec3(1.0, 1.0, rgb.r);
+        rgb = hsl2rgb(hsl);
+    }
+
+    // output pixel color
+    gl_FragColor = vec4(rgb, color.a);
 }
--- res/raw/dot_fs.glsl
+++ res/raw/dot_fs.glsl
@@ -1,6 +1,27 @@
 varying float pointSize;
+varying vec3 adjust;
 
-void main()  {
-    gl_FragColor = texture2D(UNI_Tex0, gl_PointCoord);
-    gl_FragColor.a = pointSize;
+// inspired by http://www.chilliant.com/rgb2hsv.html
+vec3 hsl2rgb(vec3 hsl)
+{
+    float r = abs(hsl.x * 6.0 - 3.0) - 1.0;
+    float g = 2.0 - abs(hsl.x * 6.0 - 2.0);
+    float b = 2.0 - abs(hsl.x * 6.0 - 4.0);
+    vec3 rgb = clamp(vec3(r, g, b), 0.0, 1.0);
+    float chroma = (1.0 - abs(2.0 * hsl.z - 1.0)) * hsl.y;
+    return (rgb - 0.5) * chroma + hsl.z;
+}
+
+void main() {
+    vec3 rgb = texture2D(UNI_Tex0, gl_PointCoord).rgb;
+
+    if (adjust.x >= 0.0) {
+        // rgb is already greyscale in that case, so r = g = b
+        float value = max(rgb.r, max(rgb.g, rgb.b));
+        vec3 hsl = adjust * vec3(1.0, 1.0, rgb.r);
+        rgb = hsl2rgb(hsl);
+    }
+
+    // output pixel color
+    gl_FragColor = vec4(rgb, pointSize);
 }
